faa000733e6227d2a15fa88d573feacd7f7069bf
Nick Fitton <nick.fitton@ovo.com>
2022-06-30 09:50:23 +0100

    lazy loading

--- host/Root.js
+++ host/Root.js
@@ -2,31 +2,14 @@ import * as React from "react";
 import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { ChunkManager } from "@callstack/repack/client";
 import { ReanimatedComponent } from "./ReanimatedComponent";
-import App1 from "app1/App.js";
-import App2 from "app2/App.js";
-
-async function loadComponent(scope, module) {
-  // Initializes the share scope. This fills it with known provided modules from this build and all remotes
-  await __webpack_init_sharing__("default");
-  // Download and execute container
-  // Query Reg here to resolve the semver range to a specific version and get a unique scope for that version
-  await ChunkManager.loadChunk(scope, "main");
-
-  const container = self[scope];
-
-  // Initialize the container, it may provide shared modules
-  await container.init(__webpack_share_scopes__.default);
-  const factory = await container.get(module);
-  const exports = factory();
-  return exports;
-}
+const App1 = React.lazy(() => import("app1/App.js"));
+const App2 = React.lazy(() => import("app2/App.js"));
 
 function App1Wrapper() {
   return (
     <React.Suspense
-      fallback={<Text style={{ textAlign: "center" }}>Loading...</Text>}
+      fallback={<Text style={{ textAlign: "center" }}>Loading app1...</Text>}
     >
       <App1 />
     </React.Suspense>
@@ -36,7 +19,7 @@ function App1Wrapper() {
 function App2Wrapper() {
   return (
     <React.Suspense
-      fallback={<Text style={{ textAlign: "center" }}>Loading...</Text>}
+      fallback={<Text style={{ textAlign: "center" }}>Loading app2...</Text>}
     >
       <App2 />
     </React.Suspense>
--- host/bootstrap.js
+++ host/bootstrap.js
@@ -1,10 +1,17 @@
 import * as React from "react";
+import { Text } from "react-native";
 
 const Root = React.lazy(() => import("./Root"));
 
 export default function Bootstrap() {
   return (
-    <React.Suspense fallback={null}>
+    <React.Suspense
+      fallback={
+        <Text style={{ textAlign: "center", marginTop: 40 }}>
+          Loading the app...
+        </Text>
+      }
+    >
       <Root />
     </React.Suspense>
   );
