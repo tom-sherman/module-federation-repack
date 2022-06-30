3b8ad76924db62e1aff8f8b96380bf773e08a9cd
Tom Sherman <the.tomsherman@gmail.com>
2022-06-30 10:35:00 +0100

    Sync import of App1 for demo purposes

--- host/Root.js
+++ host/Root.js
@@ -3,18 +3,8 @@ import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ReanimatedComponent } from "./ReanimatedComponent";
-const App1 = React.lazy(() => import("app1/App.js"));
+import App1 from 'app1/App.js';
 const App2 = React.lazy(() => import("app2/App.js"));
-
-function App1Wrapper() {
-  return (
-    <React.Suspense
-      fallback={<Text style={{ textAlign: "center" }}>Loading app1...</Text>}
-    >
-      <App1 />
-    </React.Suspense>
-  );
-}
 
 function App2Wrapper() {
   return (
@@ -33,7 +23,7 @@ export default function Root() {
     <NavigationContainer>
       <ReanimatedComponent backgroundColor="red" />
       <Tab.Navigator initialRouteName="App1">
-        <Tab.Screen name="App1" component={App1Wrapper} />
+        <Tab.Screen name="App1" component={App1} />
         <Tab.Screen name="App2" component={App2Wrapper} />
       </Tab.Navigator>
     </NavigationContainer>
